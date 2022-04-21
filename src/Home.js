3af9da99dbb433bf8e9aefbc0b098ae31e812ee5
Elizabeth <elizabethtreahy@gmail.com>
2022-04-21 16:30:08 -0400

    home page load

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
 
 function Home() {
+  const [state, setState] = useState("")
+  const [timer, setTimer] = useState(0)
+
+  const helloworld = 'HELLO, feelings.'.split('')
+  
+ 
+  useEffect(() => {
+    setTimeout(() => {
+      
+      setState(`${state}${helloworld[timer]}`)
+      timer < helloworld.length - 1 ? setTimer(timer+1) : console.log('done')
+    }, 800)
+  }, [timer])
+
+  
+  console.log('state', state)
   return (
+
     <div style={{
       position: 'absolute', left: '50%', top: '50%',
       transform: 'translate(-50%, -50%)',
@@ -11,12 +28,12 @@ function Home() {
       display: 'flex',
       flexDirection: 'column'
     }}>
-      <div style={{flexDirection: 'row'}}>
-        <h1 ><Link style={{ textDecoration: 'none' }} to="/post">HELLO WORLD</Link></h1>
+      <div style={{ flexDirection: 'row' }}>
+        <h1 ><Link style={{ textDecoration: 'none' }} to="/post">{state.length !== helloworld.length ? `${state}_` : state}</Link></h1>
       </div>
-     
-        <span style={{textAlign: 'center', flexDirection: 'row', width: '400px'}}>the journal for software engineers</span>
-     
+
+      <span style={{ textAlign: 'center', flexDirection: 'row', width: '400px' }}>{state.length !== helloworld.length ? `` : 'the journal for software engineers'}</span>
+
     </div>
   )
 }
